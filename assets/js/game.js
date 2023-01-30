2f4f3a72f70d494bfe236dedd63330e4acad44f9
Evan <evan.g.werner@gmail.com>
2023-01-30 18:05:21 -0500

    adding beginning code

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight is about to begin!")
+};
+
+//fight();
+console.log(10 + 10);
+
+//Name confirmation
+console.log("Player name is " + playerName);
